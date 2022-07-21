062cbd8adebb3f7b5656b8a47dfd1c245ce72e71
fernandowin <nando_filipe@hotmail.com>
2022-07-21 20:02:44 -0300

    feat: test count ocurrences

--- src/insights.py
+++ src/insights.py
@@ -50,20 +50,6 @@ def matches_salary_range(job, salary):
 
 
 def filter_by_salary_range(jobs, salary):
-    """Filters a list of jobs by salary range
-
-    Parameters
-    ----------
-    jobs : list
-        The jobs to be filtered
-    salary : int
-        The salary to be used as filter
-
-    Returns
-    -------
-    list
-        Jobs whose salary range contains `salary`
-    """
     list_salary = []
     for job in jobs:
         try:

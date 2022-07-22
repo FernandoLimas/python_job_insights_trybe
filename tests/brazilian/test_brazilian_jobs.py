c4e8f98ea12a88b09c1527b3e850278ca7399392
fernandowin <nando_filipe@hotmail.com>
2022-07-21 21:14:20 -0300

    feat: test brazilian jobs

--- tests/brazilian/test_brazilian_jobs.py
+++ tests/brazilian/test_brazilian_jobs.py
@@ -1,5 +1,7 @@
-# from src.brazilian_jobs import read_brazilian_file
+from src.brazilian_jobs import read_brazilian_file
 
 
 def test_brazilian_jobs():
-    pass
+    header, *tail = read_brazilian_file("tests/mocks/brazilians_jobs.csv")
+    keys = ["title", "salary", "type"]
+    assert all((key in keys) for key in header)

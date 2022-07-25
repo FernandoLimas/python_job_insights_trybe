89cc763037f95fa6fabc0380607536a991c157b5
fernandowin <nando_filipe@hotmail.com>
2022-07-25 17:09:28 -0300

    feat: routes and views

--- src/routes_and_views.py
+++ src/routes_and_views.py
@@ -15,6 +15,7 @@ from .more_insights import (
     slice_jobs,
     get_int_from_args,
     build_jobs_urls,
+    get_job,
 )
 
 bp = Blueprint("client", __name__, template_folder="templates")
@@ -72,6 +73,13 @@ def list_jobs():
     }
 
     return render_template("list_jobs.jinja2", ctx=ctx)
+
+
+@bp.route("/job/<index>")
+def job(index):
+    jobs = read("./src/jobs.csv")
+    getJobs = get_job(jobs, index)
+    return render_template("job.jinja2", job=getJobs)
 
 
 def init_app(app: Flask):
